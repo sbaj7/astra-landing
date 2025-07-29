90dd2dccd78a1f5de447ebd5cbc2c005364434f1
sbaj7 <147559149+sbaj7@users.noreply.github.com>
2025-07-28 22:37:28 -0400

    Update AstraApp.jsx

--- src/components/AstraApp.jsx
+++ src/components/AstraApp.jsx
@@ -964,7 +964,7 @@ const InputBar = ({
 
   return (
     <div style={{
-      padding: '16px 0 4px 0',
+      padding: '8px 0 4px 0',
       paddingBottom: 'max(8px, env(safe-area-inset-bottom))',
       backgroundColor: theme.backgroundSurface,
       borderTopLeftRadius: '20px',
